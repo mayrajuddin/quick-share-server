d6e0f1d21fe3d4f9c1ed8ac39c9cef3f6d09e622
uddinmayraj <mayrajuddin03@gmail.com>
2022-12-30 12:08:56 +0600

    add user, save posts to db

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
+require('dotenv').config()
 const port = process.env.PORT || 5000
 
 
@@ -11,6 +13,29 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Quick Share Server')
 })
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oejruqx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const usersCollection = client.db("quickShare").collection("users");
+        const postsCollection = client.db("quickShare").collection("posts");
+        // insert user
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+        // save post to db
+        app.post('/posts', async (req, res) => {
+            const post = req.body
+            const result = await postsCollection.insertOne(post)
+            res.send(result)
+        })
+    }
+    finally { }
+}
+run().catch(err => console.log(err))
 
 app.listen(port, () => {
     console.log(`quick share server running on  ${port}`);
